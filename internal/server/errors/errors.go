6cebb6db921c21714241c81feb6b9408e7feac04
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-03 00:50:49 +0300

    OZON-24 Fix errors struct

--- internal/server/errors/errors.go
+++ internal/server/errors/errors.go
@@ -2,7 +2,6 @@ package errors
 
 import (
 	"fmt"
-	"net/http"
 )
 
 type Error struct {
@@ -17,31 +16,24 @@ func (err Error) Error() string {
 
 var (
 	ErrUserUnauthorized error = Error{
-		HttpCode: http.StatusUnauthorized,
-		Message:  "user is unauthorized",
+		Message: "user is unauthorized",
 	}
 	ErrInternalError error = Error{
-		HttpCode: http.StatusInternalServerError,
-		Message:  "something went wrong",
+		Message: "something went wrong",
 	}
 	ErrUserNotFound error = Error{
-		HttpCode: http.StatusInternalServerError,
-		Message:  "user not found",
+		Message: "user not found",
 	}
 	ErrSessionNotFound error = Error{
-		HttpCode: http.StatusInternalServerError,
-		Message:  "something went wrong",
+		Message: "something went wrong",
 	}
 	ErrIncorrectUserEmail error = Error{
-		HttpCode: http.StatusBadRequest,
-		Message:  "incorrect user email",
+		Message: "incorrect user email",
 	}
 	ErrIncorrectUserPassword error = Error{
-		HttpCode: http.StatusBadRequest,
-		Message:  "incorrect user password",
+		Message: "incorrect user password",
 	}
 	ErrEmailAlreadyExist error = Error{
-		HttpCode: http.StatusConflict,
-		Message:  "user email already exist",
+		Message: "user email already exist",
 	}
 )
