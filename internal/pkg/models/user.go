e23f19d9ffa69a66208e545c6c0eade5fc15b883
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-02 23:55:43 +0300

    OZON-24 Add struct user

--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -1,1 +1,20 @@
 package models
+
+type ProfileUser struct {
+	Id        uint64 `json:"-"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	Avatar    string `json:"avatar"`
+}
+
+type LoginUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SignupUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
