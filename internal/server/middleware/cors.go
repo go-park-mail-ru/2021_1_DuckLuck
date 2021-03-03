eb8093d4b62f80312cd7d7c6462b7df108a67072
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-03 13:28:20 +0300

    OZON-28 Add middlewares

---
+++ internal/server/middleware/cors.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, "+
+			"Accept-Encoding, Authorization")
+
+		// If method of request is only for get options
+		if r.Method == http.MethodOptions {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
