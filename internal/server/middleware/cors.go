5b0e638ff87a43a0caf8f0a9d1c3638530a4cd26
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-05 08:59:02 +0300

    OZON-29 Fix cors

--- internal/server/middleware/cors.go
+++ internal/server/middleware/cors.go
@@ -2,11 +2,13 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/go-park-mail-ru/2021_1_DuckLuck/configs"
 )
 
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Origin", configs.CorsOrigins)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		// If method of request is only for get options
 		if r.Method == http.MethodOptions {
