32b9d1eb301e5af783db1272e43ef6824657a660
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-05 11:46:14 +0300

    OZON-29 Add file response

--- internal/server/tools/http_utils.go
+++ internal/server/tools/http_utils.go
@@ -1,7 +1,9 @@
 package tools
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -9,6 +11,11 @@ func SetJSONResponse(w http.ResponseWriter, body []byte, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	w.Write(body)
+}
+
+func SetFileResponse(w http.ResponseWriter, file *os.File, statusCode int) {
+	w.WriteHeader(statusCode)
+	io.Copy(w, file)
 }
 
 func SetCookie(w http.ResponseWriter, cookieValue string, cookieName string, duration time.Duration) {
