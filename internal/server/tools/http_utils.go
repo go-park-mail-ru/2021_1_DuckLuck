139d415979a0991b43cbee4d8fbb465a0e989277
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-05 09:00:38 +0300

    OZON-29 Fix response parser

--- internal/server/tools/http_utils.go
+++ internal/server/tools/http_utils.go
@@ -5,10 +5,10 @@ import (
 	"time"
 )
 
-func SetJSONResponse(w http.ResponseWriter, jsonStr string, statusCode int) {
+func SetJSONResponse(w http.ResponseWriter, body []byte, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	w.Write([]byte(jsonStr))
+	w.Write(body)
 }
 
 func SetCookie(w http.ResponseWriter, cookieValue string, cookieName string, duration time.Duration) {
