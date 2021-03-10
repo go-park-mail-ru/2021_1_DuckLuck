1daded54c8dc846da97240afe449d78372cdad48
DuckLuckBreakout <mrdivan07@gmail.com>
2021-03-11 00:00:19 +0300

    OZON-45 Add tests for product, session and user pkg

--- internal/pkg/session/repository.go
+++ internal/pkg/session/repository.go
@@ -4,6 +4,9 @@ import (
 	"github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/models"
 )
 
+//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/session Repository
+
+
 type Repository interface {
 	Add(session *models.Session) error
 	GetByValue(sessionValue string) (*models.Session, error)
