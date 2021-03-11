e0edf581cb89a573d4f72a28827a84213c7d58d7
mortawe <53974734+mortawe@users.noreply.github.com>
2021-03-11 11:00:46 +0300

    Revert "Ozon 45 tests"

--- internal/pkg/product/repository.go
+++ internal/pkg/product/repository.go
@@ -4,8 +4,6 @@ import (
 	"github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/models"
 )
 
-//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/product Repository
-
 type Repository interface {
 	GetById(productId uint64) (*models.Product, error)
 	GetListPreviewProducts(paginator *models.PaginatorProducts) (*models.RangeProducts, error)
--- internal/pkg/product/usecase.go
+++ internal/pkg/product/usecase.go
@@ -1,6 +1,4 @@
 package product
 
-//go:generate mockgen -destination=./mock/mock_usecase.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/product UseCase
-
 type UseCase interface {
 }
