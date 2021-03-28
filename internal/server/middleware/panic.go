b3d9fcb03880a342867df8fe82028c0fd2654984
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-03-28 10:21:54 +0300

    OZON-53 Fix response in middlewares

--- internal/server/middleware/panic.go
+++ internal/server/middleware/panic.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/go-park-mail-ru/2021_1_DuckLuck/internal/server/errors"
 	"net/http"
 
 	"github.com/go-park-mail-ru/2021_1_DuckLuck/internal/server/tools"
@@ -10,7 +11,7 @@ func Panic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				tools.SetJSONResponse(w, []byte("{\"error\": \"something went wrong\"}"), http.StatusBadRequest)
+				tools.SetJSONResponse(w, errors.ErrBadRequest, http.StatusBadRequest)
 				return
 			}
 		}()
