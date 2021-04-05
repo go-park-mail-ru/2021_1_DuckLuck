f108089c3fed1a16b3e3423e71121f4ccf9f0d9a
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-05 20:41:57 +0300

    OZON-34 Fix hasher functions

--- internal/server/tools/hasher/hasher.go
+++ internal/server/tools/hasher/hasher.go
@@ -1,4 +1,4 @@
-package password_hasher
+package hasher
 
 import (
 	"bytes"
@@ -9,27 +9,62 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-const (
-	times   = 1
-	memory  = 64 * 1024
-	threads = 4
-	keyLen  = 32
-	saltLen = 8
+type Settings struct {
+	times   uint32
+	memory  uint32
+	threads uint8
+	keyLen  uint32
+	saltLen int
+}
+
+var (
+	passwordSettings = &Settings{
+		times:   1,
+		memory:  64 * 1024,
+		threads: 4,
+		keyLen:  64,
+		saltLen: 8,
+	}
+	csrfTokenSettings = &Settings{
+		times:   1,
+		memory:  1024,
+		threads: 4,
+		keyLen:  32,
+		saltLen: 4,
+	}
 )
 
 func GenerateHashFromPassword(password string) ([]byte, error) {
-	salt := make([]byte, saltLen)
+	return generateHashFromSecret(password, passwordSettings)
+}
+
+func CompareHashAndPassword(hash []byte, password string) bool {
+	return compareHashAndSecret(hash, password, passwordSettings)
+}
+
+func GenerateHashFromCsrfToken(password string) ([]byte, error) {
+	return generateHashFromSecret(password, csrfTokenSettings)
+}
+
+func CompareHashAndCsrfToken(hash []byte, password string) bool {
+	return compareHashAndSecret(hash, password, csrfTokenSettings)
+}
+
+func generateHashFromSecret(secret string, settings *Settings) ([]byte, error) {
+	salt := make([]byte, settings.saltLen)
 	_, err := rand.Read(salt)
 	if err != nil {
 		return nil, errors.ErrServerSystem
 	}
 
-	hashedPassword := argon2.IDKey([]byte(password), salt, times, memory, threads, keyLen)
+	hashedPassword := argon2.IDKey([]byte(secret), salt, settings.times, settings.memory,
+		settings.threads, settings.keyLen)
 	return append(salt, hashedPassword...), nil
 }
 
-func CompareHashAndPassword(hash []byte, password string) bool {
-	salt := hash[0:saltLen]
-	hashedPassword := argon2.IDKey([]byte(password), salt, times, memory, threads, keyLen)
-	return bytes.Equal(hashedPassword, hash[saltLen:])
+func compareHashAndSecret(hash []byte, secret string, settings *Settings) bool {
+	salt := hash[0:settings.saltLen]
+	hashedPassword := argon2.IDKey([]byte(secret), salt, settings.times, settings.memory,
+		settings.threads, settings.keyLen)
+	return bytes.Equal(hashedPassword, hash[settings.saltLen:])
 }
