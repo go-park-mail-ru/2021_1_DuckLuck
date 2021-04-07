7bcfe1f4a9539935b1080a576241e76210375dc9
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-04-08 00:29:02 +0300

    OZON-61 Change upload file function target

--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.15
 require (
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/aws/aws-sdk-go v1.38.15
+	github.com/aws/aws-sdk-go-v2 v1.3.1
+	github.com/aws/aws-sdk-go-v2/config v1.1.4
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.4.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/mock v1.5.0
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.10.0
 	github.com/lithammer/shortuuid v3.0.0+incompatible
 	github.com/microcosm-cc/bluemonday v1.0.5
@@ -18,6 +23,7 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- internal/server/tools/file_utils/file_utils.go
+++ internal/server/tools/file_utils/file_utils.go
@@ -1,14 +1,37 @@
 package file_utils
 
 import (
-	"io"
-	"net/http"
-	"os"
-
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/go-park-mail-ru/2021_1_DuckLuck/internal/server/errors"
+	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	uuid "github.com/satori/go.uuid"
 )
+
+var sessionS3 *session.Session
+
+func init() {
+	accessKeyID := ""
+	secretAccessKey := ""
+	myRegion := "ap-northeast-1"
+	var err error
+	sessionS3, err = session.NewSession(
+		&aws.Config{
+			Region: aws.String(myRegion),
+			Endpoint: aws.String("hb.bizmrg.com"),
+			Credentials: credentials.NewStaticCredentials(
+				accessKeyID,
+				secretAccessKey,
+				"",
+			),
+		})
+	if err != nil {
+		panic(err)
+	}
+}
 
 func UploadFile(r *http.Request, fileKey string, pathToUpload string) (string, error) {
 	// Max size - 10 Mb
@@ -32,16 +55,13 @@ func UploadFile(r *http.Request, fileKey string, pathToUpload string) (string, e
 	}
 
 	newName := uuid.NewV4().String() + fileType
-	newFile, err := os.Create(pathToUpload + newName)
-	if err != nil {
-		return "", errors.ErrServerSystem
-	}
-	defer newFile.Close()
+	uploader := s3manager.NewUploader(sessionS3)
 
-	_, err = io.Copy(newFile, file)
-	if err != nil {
-		return "", errors.ErrServerSystem
-	}
-
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("duckluckmarket"),
+		ACL:    aws.String("public-read"),
+		Key:    aws.String("test_file_name"),
+		Body:   file,
+	})
 	return newName, nil
 }
