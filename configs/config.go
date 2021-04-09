9dfba9c1a5d11c33dcaacf57c1eaee7174ecc10f
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-09 23:02:58 +0300

    OZON-61 Add path to env

--- configs/config.go
+++ configs/config.go
@@ -12,5 +12,10 @@ var (
 	PathToLogFile = PathProject + "/log/log.txt"
 	LogLevel      = "debug"
 
+	PathToApiServerEnv  = PathProject + "/configs/envs/api_server.env"
+	PathToPostgreSqlEnv = PathProject + "/configs/envs/postgresql.env"
+	PathToRedisEnv      = PathProject + "/configs/envs/redis.env"
+	PathToS3Env         = PathProject + "/configs/envs/s3.env"
+
 	CorsOrigin = "http://localhost:3000"
 )
