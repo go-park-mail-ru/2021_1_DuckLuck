3b4fc595d467c45d113e5acfd763f1de411ed38c
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-12 22:10:49 +0300

    OZON-67 Add user mock

--- internal/pkg/user/handler.go
+++ internal/pkg/user/handler.go
@@ -4,6 +4,7 @@ import "net/http"
 
 //go:generate mockgen -destination=./mock/mock_handler.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/user Handler
 
+
 type Handler interface {
 	Login(w http.ResponseWriter, r *http.Request)
 	UpdateProfile(w http.ResponseWriter, r *http.Request)
