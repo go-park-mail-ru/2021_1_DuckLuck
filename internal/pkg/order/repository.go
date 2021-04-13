bd053d947e71105fc9b9b24beeef1592b2e57bf7
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-13 10:34:00 +0300

    OZON-67 Add order mocks

--- internal/pkg/order/repository.go
+++ internal/pkg/order/repository.go
@@ -2,6 +2,8 @@ package order
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/order Repository
+
 type Repository interface {
 	AddOrder(order *models.Order, userId uint64, products []*models.PreviewCartArticle,
 		price *models.TotalPrice) (uint64, error)
