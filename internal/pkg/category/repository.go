ca4af1509d7d9b0a6ce2f4e7b19aa022499f5f1c
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-13 11:22:02 +0300

    OZON-67 Add category mocks

--- internal/pkg/category/repository.go
+++ internal/pkg/category/repository.go
@@ -2,8 +2,11 @@ package category
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/category Repository
+
 type Repository interface {
 	GetNextLevelCategories(categoryId uint64) ([]*models.CategoriesCatalog, error)
 	GetCategoriesByLevel(level uint64) ([]*models.CategoriesCatalog, error)
 	GetAllSubCategoriesId(categoryId uint64) ([]uint64, error)
+	GetPathToCategory(categoryId uint64) ([]*models.CategoriesCatalog, error)
 }
--- internal/pkg/category/usecase.go
+++ internal/pkg/category/usecase.go
@@ -2,6 +2,8 @@ package category
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_usecase.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/internal/pkg/category UseCase
+
 type UseCase interface {
 	GetCatalogCategories() ([]*models.CategoriesCatalog, error)
 	GetSubCategoriesById(categoryId uint64) ([]*models.CategoriesCatalog, error)
