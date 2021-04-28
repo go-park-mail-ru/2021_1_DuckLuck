99f227ac88d550c2d27b9f5e0790da7305791c50
Suvorov_Nikita <suvnik.2020@gmail.com>
2021-04-28 09:58:10 +0300

    OZON-87 Add review interface

--- internal/pkg/review/handler.go
+++ internal/pkg/review/handler.go
@@ -1,4 +1,10 @@
 package review
 
+import "net/http"
+
 type Handler interface {
+	GetReviewStatistics(w http.ResponseWriter, r *http.Request)
+	AddNewReview(w http.ResponseWriter, r *http.Request)
+	GetReviewsForProduct(w http.ResponseWriter, r *http.Request)
+	CheckReviewRights(w http.ResponseWriter, r *http.Request)
 }
