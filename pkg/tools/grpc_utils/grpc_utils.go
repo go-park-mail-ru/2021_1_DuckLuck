212545fe7a58b1e4bc91d771f3f1918ddce5ab5a
Suvorov Nikita <suvnik.2020@gmail.com>
2021-05-04 21:30:45 +0300

    OZON-105 Fix auth interceptor in grpc utils

--- pkg/tools/grpc_utils/grpc_utils.go
+++ pkg/tools/grpc_utils/grpc_utils.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-park-mail-ru/2021_1_DuckLuck/pkg/metrics"
 	"github.com/go-park-mail-ru/2021_1_DuckLuck/pkg/tools/logger"
 
 	"github.com/lithammer/shortuuid"
@@ -16,21 +17,35 @@ var (
 	RequireIdKey = "require_key"
 )
 
-func AuthInterceptor(ctx context.Context, req interface{},
-	info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	start := time.Now()
-	md, _ := metadata.FromIncomingContext(ctx)
+func AccessInterceptor(metric *metrics.Metrics) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{},
+		info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		start := time.Now()
+		md, _ := metadata.FromIncomingContext(ctx)
 
-	requireId := shortuuid.New()
-	logger.GrpcAccessLogStart(info.FullMethod, requireId,
-		fmt.Sprintf("%v", req), fmt.Sprintf("%v", md))
+		statusCode := "200"
+		requireId := shortuuid.New()
+		logger.GrpcAccessLogStart(info.FullMethod, requireId,
+			fmt.Sprintf("%v", req), fmt.Sprintf("%v", md))
 
-	newContext := context.WithValue(ctx, RequireIdKey, requireId)
-	reply, err := handler(newContext, req)
+		newContext := context.WithValue(ctx, RequireIdKey, requireId)
+		reply, err := handler(newContext, req)
 
-	logger.GrpcAccessLogEnd(info.FullMethod, requireId,
-		fmt.Sprintf("%v", reply), start)
-	return reply, err
+		if err != nil {
+			statusCode = "500"
+			metric.Errors.WithLabelValues(statusCode, info.FullMethod, info.FullMethod).Inc()
+		} else {
+			metric.AccessHits.WithLabelValues(statusCode, info.FullMethod, info.FullMethod).Inc()
+		}
+
+		metric.Durations.WithLabelValues(statusCode, info.FullMethod,
+			info.FullMethod).Observe(time.Since(start).Seconds())
+		logger.GrpcAccessLogEnd(info.FullMethod, requireId,
+			fmt.Sprintf("%v", reply), start)
+		metric.TotalHits.Inc()
+
+		return reply, err
+	}
 }
 
 func MustGetRequireId(ctx context.Context) string {
