772a63d0a793fee1822f1f83406a8c551e9e66be
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-05-06 12:30:20 +0300

    OZON-104 Add mocks for cart

--- services/cart/pkg/cart/repository.go
+++ services/cart/pkg/cart/repository.go
@@ -2,6 +2,8 @@ package cart
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/services/cart/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/services/cart/pkg/cart Repository
+
 type Repository interface {
 	SelectCartById(userId uint64) (*models.Cart, error)
 	AddCart(userId uint64, userCart *models.Cart) error
--- services/cart/pkg/cart/usecase.go
+++ services/cart/pkg/cart/usecase.go
@@ -2,6 +2,8 @@ package cart
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/services/cart/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_usecase.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/services/cart/pkg/cart UseCase
+
 type UseCase interface {
 	AddProduct(userId uint64, cartArticle *models.CartArticle) error
 	DeleteProduct(userId uint64, identifier *models.ProductIdentifier) error
