e48df997e7e180a2e774db3d1aeed27eca99218e
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-05-06 12:40:25 +0300

    OZON-104 Add mocks for session

--- services/session/pkg/session/repository.go
+++ services/session/pkg/session/repository.go
@@ -2,6 +2,8 @@ package session
 
 import "github.com/go-park-mail-ru/2021_1_DuckLuck/services/session/pkg/models"
 
+//go:generate mockgen -destination=./mock/mock_repository.go -package=mock github.com/go-park-mail-ru/2021_1_DuckLuck/services/session/pkg/session Repository
+
 type Repository interface {
 	AddSession(session *models.Session) error
 	SelectUserIdBySession(sessionValue string) (uint64, error)
